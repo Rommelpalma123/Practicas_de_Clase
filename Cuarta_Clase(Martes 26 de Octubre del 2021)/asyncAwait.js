0454c9cfe9cf0e49efd4832bfca93516c8942a48
Rommelpalma123 <rommelpalma.3000@gmail.com>
2021-10-26 10:26:05 -0500

    Cuarta_Clase(Martes 26 de Octubre del 2021)

--- Cuarta_Clase(Martes 26 de Octubre del 2021)/asyncAwait.js
+++ Cuarta_Clase(Martes 26 de Octubre del 2021)/asyncAwait.js
@@ -1,4 +1,5 @@
 
+// declaramos un arreglo llamado book donde damos atributos como id, title y idauthor
 const books = [
 
     {
@@ -18,6 +19,7 @@ const books = [
     }
 ]
 
+// creamos otro arreglo llamado authors asi mismo le damoa atributos como el id y name
 const authors = [
 
     {
@@ -34,15 +36,20 @@ const authors = [
     }
 ]
 
+// creamos una funcion de tipo asincronica la cual la llamamos search_book_by_id 
 async function search_book_by_id(id){
 
+    // creamos una variable book luego le damos el valor del arrglo para buscar un libro por su id
     const book = books.find((book) => book.id === id);
     if(!book)
     {
+        // creamos otra variable para generar un error en caso que no se encuentre el libro 
         const error = new Error();
+        // enviamos un mensaje de error 
         error.message= "book no found";
         throw error;
     }
+    // si no hoy ningun problema se retorna un libro 
     return book;
 }
 
@@ -51,14 +58,16 @@ books.forEach( async (book) =>{
     const bookk = await search_Book_by_id(1);
 })
 
-
+// creamos una funcion de tipo asincronica la cual la llamamos search_author_by_id 
 async function search_author_by_id(id){
 
+    // creamos una variable author luego le damos el valor del arrglo para buscar un author por su id
     const author = authors.find((author) =>{ return author.id === id;
     })
 
     if(!author)
     {
+        // creamos otra variable para generar un error en caso que no se encuentre el libro 
         const error = new Error();
         error.message = "author no found";
         throw error;
@@ -67,17 +76,22 @@ async function search_author_by_id(id){
 
 }
 
+// creamos una funcion anonima para generar errrores 
 (async () => 
 {
     try {
 
+        // en el try es donde se va a ejecutar todo, decimos que en search_book_by_id busque un libro definido
         const book = await search_book_by_id(1);
+        // tambine hacemos una busqueda en el arrglo de books para buscar el id del autor 
         const author = await search_author_by_id(book.idauthor)
+        // guardamos los valoren en la variable author 
         book.author = author;
+        // imprimimos libro y autor i id
         console.log(book);
 
+    // con el catch enviaremos el error, en caso de no encontrar el libro 
     } catch (error) {
         console.log(error.message);
     }
-    
 })();
--- Cuarta_Clase(Martes 26 de Octubre del 2021)/promesas.js
+++ Cuarta_Clase(Martes 26 de Octubre del 2021)/promesas.js
@@ -1,4 +1,4 @@
-
+// declaramos un arreglo llamado books donde damos atributos como id, title y idauthor
 const books = [
 
     {
@@ -17,7 +17,7 @@ const books = [
         idauthor: 3
     }
 ]
-
+// declaramos un arreglo llamado authors donde damos atributos como id, title y idauthor
 const authors = [
 
     {
@@ -33,48 +33,60 @@ const authors = [
         name: 'Julio Verne'
     }
 ]
+
+// creamos una funcion para buscar libros y damos un parametro el cual es id 
 function search_Book_by_id(id)
 {
+    // Ahora estamos trabajando con promesas las cuales me van a retornar un valor si es que existe ese id del libro 
     return new Promise((resolve, reject) => 
     {
+        // creamos una variable donde daremos el arrgle books y se buscara el id del libro
         const book = books.find((book) => book.id === id);
         if(!book) 
-        {
+        { 
+            // creamos un error en caso de no encontrar el libro 
             const error = new Error();
             error.message= "Book not found";
             reject(error)
         }
+        // si se encuentra el libro retornado el valor del libro
         resolve(book);
     })
 }
 
 
-
+// creamos una funcion para buscar autor y damos un parametro el cual es book
 function search_author_by_id(book)
 {
+    // Ahora estamos trabajando con promesas las cuales me van a retornar un valor si es que existe ese id del libro 
     return new Promise((resolve, reject) => 
     {
+        // creamos una variable donde daremos el arrgle authors y se buscara el id dentro del arreglo books 
         const author = authors.find((author) => {
             return author.id === book.idauthor
         })
         if(!author){
 
+            // creamos un error donde se conprueba si es q existe un idauthor dentro del arreglo books 
             const error = new Error();
             error.message = "Author not found";
+            // si no se cumple saltara el error que no fue encontrado el isauthor
             reject(error);
         }
+        // si todo se cumple se guardara los valoren en author y se retornara el valor que se busca
         book.author = author;
         resolve(book);    
     })
 }
 
+// damos un id de libro el cual es el 2 y decimos que nos retorne el valor del libro si es que se encuentra
 search_Book_by_id(2).then((book) => {
     return search_author_by_id(book);
-
+// con el then y catch se puede controlar errores en las promesas
 }).then ((book) => {   
     
     console.log(book)
-
+// si no se encuentra el libro saldra un error que no se encontro el libro 
 }).catch((error) => {
 
     console.log(error.message);
