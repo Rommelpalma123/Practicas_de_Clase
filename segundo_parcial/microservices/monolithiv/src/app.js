4ad7c88daf3d8a098e703be4cf5d33444246c230
Rommelpalma123 <rommelpalma.3000@gmail.com>
2021-12-14 09:53:13 -0500

    services

--- segundo_parcial/microservices/monolithiv/src/app.js
+++ segundo_parcial/microservices/monolithiv/src/app.js
@@ -1,29 +1,31 @@
 const express = require('express');
 const app = express();
 
-const response = 
-{
+const response = {
     data:[],
-    services:"all services",
-    arquitecture:"monolithiv"
+    services:"All services",
+    arquitecture:"Monolithic"
 }
 
-app.use((req, res, next) => {
-
+app.use((req,res,next)=>{
     response.data=[];
     next();
 })
 
-app.get('/api/v1/user', (req,res) =>{
-
-    response.data.push("alex", "sara", "ayme");
+//usuarios
+app.get('/api/v1/users',(req,res)=>{
+    response.data.push("Alex","Sara","Ayme");
     return res.send(response);
-} )
-
-app.get('/api/v1/books', (req,res) =>{
-
-    response.data.push("javascript", "patrones", "antipatrones");
-    res.send(response);
+})
+///libros
+app.get('/api/v1/books', (req,res)=>{
+    response.data.push("javascript guIa definitiva","patrones JavaScript","Antipatrones STUPID");
+    return res.send(response);
+})
+////carros
+app.get('/api/v1/cars', (req,res)=>{
+    response.data.push("Versa","BMW", "Kia");
+    return res.send(response);
 })
 
-module.export = app;
+module.exports = app;
